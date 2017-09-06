ce066bcc9201da01f59645d8283b21987637021a
Braydon Squire <guitarf1@hotmail.com>
2017-09-06 17:58:11 -0600

    First commit

--- classes.js
+++ classes.js
@@ -20,7 +20,17 @@ Each employee can:
     IE - "Dave Smith Widget"
 
 call your class Employee and receive all the data in the constructor in the order listed
-*/
+*/class Employee {
+    constructor(first_name,last_name,email,age){
+this.first_name = first_name;
+this.last_name = last_name;
+this.email = email;
+this.age = age;
+    }
+    makeWidget(){
+        return this.first_name + " " + this.last_name + " Widget";
+    }
+}
 
 
 
@@ -40,7 +50,21 @@ They can (methods) :
 call your class Manager
 
 */
-
+class Manager{
+    constructor(first_name,last_name,email,age,reports){
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.age = age;
+        this.reports = reports =[];
+    }
+    hire(){
+        this.reports.unshift(this.employee);
+    }
+    fire(index){
+ delete this.Employee[i];
+    }
+}
 
 
 
